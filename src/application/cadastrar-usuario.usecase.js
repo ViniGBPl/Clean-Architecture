3503c5ef95f55b3ea180231824245c86e4456ce6
unknown <viniciusgabrielss10@gmail.com>
2026-09-15 23:55:52 -0300

    criação do use-case

---
+++ src/application/cadastrar-usuario.usecase.js
@@ -0,0 +1,13 @@
+
+
+module.exports = function cadastrarUsuarioUseCase() {
+    return async function({nome_completo,CPF,telefone,endereco,email}){
+       await usuariosRepository.cadastrarUsuario({
+            nome_completo,
+            CPF,
+            telefone,
+            endereco,
+            email
+        });
+    };
+}
