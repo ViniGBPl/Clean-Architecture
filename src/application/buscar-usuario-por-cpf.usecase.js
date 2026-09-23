b76e59865e840b2bb639194e40deb0c83564a585
ViniGBPl <viniciusgabrielss10@gmail.com>
2026-09-23 10:40:06 -0300

    test: implementa os testes para buscar por cpf

--- src/application/buscar-usuario-por-cpf.usecase.js
+++ src/application/buscar-usuario-por-cpf.usecase.js
@@ -1,7 +1,9 @@
-const { Either } = require("../shared/errors");
+const { Either, AppError } = require("../shared/errors");
 
 module.exports = function buscarUsuarioPorCPFUseCase({ usuariosRepository}){
+    if(!usuariosRepository) throw new AppError(AppError.dependencias);
     return async function ({CPF}) {
+    if(!CPF) throw new AppError(AppError.parametrosObrigatoriosAusentes);
      const usuario = await usuariosRepository.buscarPorCPF(CPF);
      // Como estamos usando o Either, usamos ele para fazer o retorno ao inves de só usuario
      return Either.Right(usuario);
--- src/application/buscar-usuario-por-cpf.usecase.spec.js
+++ src/application/buscar-usuario-por-cpf.usecase.spec.js
@@ -1,3 +1,4 @@
+const { AppError } = require('../shared/errors');
 const buscarUsuarioPorCPFUseCase = require('./buscar-usuario-por-cpf.usecase')
 
 describe('Buscar usuario por CPF UseCase', function(){
@@ -28,6 +29,35 @@ describe('Buscar usuario por CPF UseCase', function(){
         expect(output.right).toEqual(outputDTO);
         expect(usuariosRepository.buscarPorCPF).toHaveBeenCalledWith(cpfDTO.CPF);
         expect(usuariosRepository.buscarPorCPF).toHaveBeenCalledTimes(1);
+    })
+
+    test('Deve retornar null se não existir nenhum usuário com o CPF informado', async function () {
+        usuariosRepository.buscarPorCPF.mockResolvedValue(null);
+        const cpfDTO = {
+            CPF:'CPF_nao_cadastrado'
+        }
+
+        const sut = buscarUsuarioPorCPFUseCase({usuariosRepository})
+        const output = await sut(cpfDTO);
+
+        expect(output.right).toBeNull();
+        expect(usuariosRepository.buscarPorCPF).toHaveBeenCalledWith(cpfDTO.CPF);
+        expect(usuariosRepository.buscarPorCPF).toHaveBeenCalledTimes(1);
+        
+    });
+
+    test('Deve retornar um  throw AppError se o usuariosRepository não for fornecido', function(){
+
+        expect(() => buscarUsuarioPorCPFUseCase({})).toThrow(new AppError(AppError.dependencias))
+
+    });
+
+    test('Deve retornar um throw AppError se o campo CPF obrigatorio não for fornecido',async function() {
+
+        const sut = buscarUsuarioPorCPFUseCase({usuariosRepository});
+
+        await expect(() => sut({})).rejects.toThrow(new AppError(AppError.parametrosObrigatoriosAusentes));
+        
     })
 
 
